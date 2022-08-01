39d19d33a26df0b7d4316f0f9f471d45942d202c
yang0123456789 <378525908@qq.com>
2022-08-01 10:07:01 +0800

    hot 100

--- code-demo/src/Solution.java
+++ code-demo/src/Solution.java
@@ -1,35 +1,79 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
 
 class Solution {
-    public static boolean isValid(String s) {
-        Deque<Character> queue = new LinkedList<>();
-        HashMap<Character, Character> map = new HashMap<Character, Character>() {{
-            put(')','(');
-            put(']','[');
-            put('}','{');
+    public int maximalRectangle(char[][] matrix) {
+        int row = matrix.length;
+        if (row == 0) {
+            return 0;
+        }
+        int col = matrix[0].length;
+        int[][] left = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
 
-        }};
+            }
+        }
 
-        System.out.println(map);
-        for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
-            if (cur == '(' || cur == '[' || cur == '{') {
-                queue.push(cur);
-            } else {
-                System.out.println(map.get(cur));
-                if (queue.isEmpty() || queue.pop() != map.get(cur)) {
-                    return false;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
                 }
             }
-
         }
-        return queue.isEmpty();
+
+
+        int res = 0;
+        Deque<Integer> queue = new ArrayDeque<Integer>();
+
+        for (int j = 0; j < col; j++) {
+            int[] low = new int[row];
+            int[] up = new int[row];
+            for (int i = 0; i < row; i++) {
+                while (!queue.isEmpty() && left[queue.peek()][j] >= left[i][j]) {
+                    queue.pop();
+                }
+                low[i] = queue.isEmpty() ? -1 : queue.peek();
+                queue.push(i);
+            }
+            queue.clear();
+            for (int i = row - 1; i >= 0; i--) {
+                while (!queue.isEmpty() && left[queue.peek()][j] >= left[i][j]) {
+                    queue.pop();
+                }
+                up[i] = queue.isEmpty() ? row : queue.peek();
+                queue.push(i);
+            }
+            queue.clear();
+            for (int i = 0; i < row; i++) {
+                res = Math.max(res, left[i][j] * (up[i] - low[i] - 1));
+            }
+        }
+        return res;
 
     }
 
     public static void main(String[] args) {
-        System.out.println(isValid("()"));
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++");
+        stack = new ArrayDeque<>();
+        stack.offer(1);
+        stack.offer(2);
+        stack.offer(3);
+        System.out.println(stack.poll());
+        System.out.println(stack.poll());
+        System.out.println(stack.poll());
+
+
     }
 }
